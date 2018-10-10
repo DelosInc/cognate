1195a1f7e80c3db4b42f251cae9ec8c2efab35a4
lightphantomhive <shashank152k@gmail.com>
2018-10-10 13:01:05 +0530

    Updated array_sum_updated.c

--- array_sum_updated.c
+++ array_sum_updated.c
@@ -1,5 +1,3 @@
-
-
 #include<stdio.h>
 #include<stdlib.h>
 #include "mpi.h"
@@ -12,13 +10,13 @@ int main(int argc, char *argv[]){
 
     int np, id, i, j, chunk, offset, sum, total_sum = 0;
 
-    MPI_STATUS status;
+    MPI_Status status;
 
-    int update_sum(int, int, int);
+    int update_sum(int offset, int chunk, int id);
 
     MPI_Init(&argc, &argv);
-    MPI_Comm_size(MPI_Comm_WORLD, &np);
-    MPI_Comm_rank(MPI_Comm_WORLD, &id);
+    MPI_Comm_size(MPI_COMM_WORLD, &np);
+    MPI_Comm_rank(MPI_COMM_WORLD, &id);
 
     chunk = size / np;
 
@@ -36,31 +34,31 @@ int main(int argc, char *argv[]){
 
         for(i = 1; i < np; i++){
 
-            MPI_Send(&offset, 1, MPI_INT, i, 1, MPI_Comm_WORLD);
-            MPI_Send(&data[offset], chunk, MPI_INT, i, 1, MPI_Comm_WORLD);
+            MPI_Send(&offset, 1, MPI_INT, i, 1, MPI_COMM_WORLD);
+            MPI_Send(&data[offset], chunk, MPI_INT, i, 1, MPI_COMM_WORLD);
             offset = offset + chunk;
         }
 
         sum = update_sum(0, chunk, 0);
 
-        MPI_Reduce(&sum, &total_sum, 1, MPI_INT, MPI_SUM, 0, MPI_Comm_WORLD);
+        MPI_Reduce(&sum, &total_sum, 1, MPI_INT, MPI_SUM, 0, MPI_COMM_WORLD);
         printf("\nFinal Sum = %d", total_sum);
     }
 
     else{
 
-        MPI_Recv(&offset, 1, MPI_INT, 0, 1, MPI_Comm_WORLD, &status);
-        MPI_Recv(&data[offset], chunk, MPI_INT, 0, 1, MPI_Comm_WORLD, &status);
+        MPI_Recv(&offset, 1, MPI_INT, 0, 1, MPI_COMM_WORLD, &status);
+        MPI_Recv(&data[offset], chunk, MPI_INT, 0, 1, MPI_COMM_WORLD, &status);
 
-        sum = update(offset, chunk, id);
+        sum = update_sum(offset, chunk, id);
 
-        MPI_Reduce(&sum, &total_sum, 1, MPI_INT, MPI_SUM, 0, MPI_Comm_WORLD);
+        MPI_Reduce(&sum, &total_sum, 1, MPI_INT, MPI_SUM, 0, MPI_COMM_WORLD);
     }
 
     MPI_Finalize();
 }
 
-int update(int offset, int chunk, int id){
+int update_sum(int offset, int chunk, int id){
 
     int i, sum = 0;
     for(i = offset; i < offset + chunk; i++){
